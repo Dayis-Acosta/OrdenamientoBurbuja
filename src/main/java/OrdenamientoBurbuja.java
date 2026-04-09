74c9ac3ba31338598ae43638e6109038eb5e74ed
Dayanaa Accosta <est.dayanna.acosta@unimilitar.edu.co>
2026-04-09 10:37:31 -0500

    Metodo de ordenamiento con una clase

--- src/main/java/OrdenamientoBurbuja.java
+++ src/main/java/OrdenamientoBurbuja.java
@@ -7,8 +7,28 @@ public class OrdenamientoBurbuja {
         
         System.out.println(Arrays.toString(arr));    
         
+        //Ordenar el arreglo
+       OrdenamientoBurbuja(arr);
+        //Mostrar nuevamente el arreglo ordenado
+        System.out.println(Arrays.toString(arr));
         
+       
+    }
+    
+     //Metodo Para Optimizar el codigo
+    public static void OrdenamientoBurbuja(int[]arr){
+           int n = arr.length; 
         
-        
+        for(int i = 0; i<n-1; i++){
+            for(int j = i+1; j<n; j++){
+                if(arr[i]>arr[j]){
+                    int aux = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = aux;
+                }
+                
+            }
+            
+        }  
     }
 }
