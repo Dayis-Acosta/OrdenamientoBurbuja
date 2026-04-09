f6a0df1458eb7d2ebf7c6b8da61a719d3ae65dda
Dayanaa Accosta <est.dayanna.acosta@unimilitar.edu.co>
2026-04-09 10:10:31 -0500

    inicio de proyecto con los datos que hay que ordenar

---
+++ src/main/java/OrdenamientoBurbuja.java
@@ -0,0 +1,14 @@
+//DAyanna acosta ledesma (6014503)
+import java.util. Arrays;
+public class OrdenamientoBurbuja {
+
+    public static void main(String[] args) {
+        int [] arr ={45, 22, 11, -4, 7, 3};
+        
+        System.out.println(Arrays.toString(arr));    
+        
+        
+        
+        
+    }
+}
